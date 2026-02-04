c8bccb04dd3a59a12cdb9f8643b035d824883e65
inforoketgain-netizen <info.roketgain@gmail.com>
2026-02-04 14:20:12 +0100

    add missing ReferralInput component

---
+++ src/components/ReferralInput.tsx
@@ -0,0 +1,12 @@
+export default function ReferralInput() {
+  return (
+    <div>
+      <label>Code de parrainage</label>
+      <input
+        type="text"
+        placeholder="Entre ton code de parrainage"
+        className="border rounded p-2 w-full mt-1"
+      />
+    </div>
+  );
+}
